03a7f36fb5dcd57b539970ba8334761e71af7a2f
Brent Broeckx <r0738889@student.thomasmore.be>
2022-06-07 18:38:37 +0200

    possible safari fix

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -62,12 +62,9 @@ function ProjectCard(props) {
                             <span className='text-base mt-3'>{truncate(props.project.what)}</span>
                         </div>
                         <div>
-                            <Link href={"/project/" + props.project.id} >
-                                <button onClick={() => routeToProject(props.project.id)} className="bg-transparent cursor-pointer w-max hover:bg-orange-500 text-orange-400 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">
-                                    Lees meer
-                                </button>
-                            </Link>
-                            
+                            <a href={"/project/" + props.project.id} className="bg-transparent cursor-pointer w-max hover:bg-orange-500 text-orange-400 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">
+                                Lees meer
+                            </a>
                         </div>
                         
 
